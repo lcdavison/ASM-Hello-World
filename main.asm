3dc756122acdbb40cff7bf5d4b3f43bef6b17031
Luke Davison <luke.davison@talktalk.net>
2018-03-20 13:00:40 +0000

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,29 @@
+section .text
+    
+	global _start
+    
+
+_start:
+    
+	mov edx,len ; Move length of msg to EDX reg
+    
+	mov ecx,msg ; Move msg to ECX reg
+    
+	mov ebx,1
+    
+	mov eax,4
+    
+	int 0x80
+
+    
+    
+	mov eax,1
+    
+	int 0x80
+    
+
+section .data
+
+msg db 'Hello World!',0xa
+
+len equ $ - msg
